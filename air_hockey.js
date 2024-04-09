efa33b810cc63a41fe99fd3cbc570ac61e6446eb
June Sun <june.sun@yale.edu>
2024-04-09 16:55:10 -0400

    Rink and scoreboard design

--- air_hockey.js
+++ air_hockey.js
@@ -1,7 +1,20 @@
+// Colors and sizes
+const RED = 0xed4a4a
+const BLUE = 0x1239b0
+const NAVY = 0x000080
+const YELLOW = 0xFFFF00
+const SILVER = 0xd9d9d9
+const BLACK = 0x000000
+const ICE_WHITE = 0xebeff5
+const DARK_GRAY = 0x303030
+
+const CIRCLE_RAD = 60
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 400,
+    backgroundColor: ICE_WHITE,
     physics: {
         default: 'arcade',
         arcade: {
@@ -17,9 +30,9 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var puck, paddle1, paddle2, cursors, wasdKeys;
+var puck, paddle1, paddle2, cursors, wasdKeys, scoreText;
 var player1Score = 0, player2Score = 0;
-var scoreMargin = 100; // Margin for scoring
+var scoreMargin = 25; // Margin for scoring
 
 function preload() {
     this.load.css('my_styles', 'styles.css');
@@ -28,6 +41,51 @@ function preload() {
 }
 
 function create() {
+    // Rink setup
+    const center_line = this.add.line(0, 0, this.game.config.width / 2, 0, this.game.config.width / 2, this.game.config.height * 2, RED)
+    center_line.setStrokeStyle(4, RED)
+    center_line.setLineWidth(3)
+
+    const neutral_zone1 = this.add.line(0, 0, this.game.config.width / 2 - 90, 0, this.game.config.width / 2 - 90, this.game.config.height * 2, BLUE)
+    neutral_zone1.setStrokeStyle(4, BLUE)
+    neutral_zone1.setLineWidth(3)
+
+    const neutral_zone2 = this.add.line(0, 0, this.game.config.width / 2 + 90, 0, this.game.config.width / 2 + 90, this.game.config.height * 2, BLUE)
+    neutral_zone2.setStrokeStyle(4, BLUE)
+    neutral_zone2.setLineWidth(3)
+
+    const goal_line1 = this.add.line(0, 0, 25, 0, 25, this.game.config.height * 2, RED)
+    goal_line1.setStrokeStyle(4, RED)
+    goal_line1.setLineWidth(3)
+
+    const goal_line2 = this.add.line(0, 0, this.game.config.width - 25, 0, this.game.config.width - 25, this.game.config.height * 2, RED)
+    goal_line2.setStrokeStyle(4, RED)
+    goal_line2.setLineWidth(3)
+
+    const center_circle = this.add.graphics();
+    center_circle.lineStyle(1, BLUE, 1);
+
+    center_circle.beginPath();
+    // arc (x, y, radius, start_angle, end_angle, anticlockwise_bool)
+    center_circle.arc(this.game.config.width / 2, this.game.config.height / 2, CIRCLE_RAD, Phaser.Math.DegToRad(0), Phaser.Math.DegToRad(360), false);
+    center_circle.strokePath();
+
+    const c1 = this.add.arc(this.game.config.width / 5, this.game.config.height / 4, CIRCLE_RAD, 0, 360, false);
+    c1.setStrokeStyle(1, RED);
+    const c2 = this.add.arc(this.game.config.width / 5, this.game.config.height * 3 / 4, CIRCLE_RAD, 0, 360, false);
+    c2.setStrokeStyle(1, RED);
+    const c3 = this.add.arc(this.game.config.width * 4 / 5, this.game.config.height / 4, CIRCLE_RAD, 0, 360, false);
+    c3.setStrokeStyle(1, RED);
+    const c4 = this.add.arc(this.game.config.width * 4 / 5, this.game.config.height * 3 / 4, CIRCLE_RAD, 0, 360, false);
+    c4.setStrokeStyle(1, RED);
+
+    // Scoreboard setup
+    var score_rect = this.add.rectangle(this.game.config.width / 2, 0, 120, 120, SILVER);
+    var p1_rect = this.add.rectangle(this.game.config.width / 2 - 40, 0, 40, 25, RED)
+    var p2_rect = this.add.rectangle(this.game.config.width / 2 + 40, 0, 40, 25, BLUE)
+    var score_label = this.add.text(this.game.config.width / 2, 2, 'SCORE', { fontSize: '10px', fill: '0x000000' }).setOrigin(0.5, 0);
+
+    // Game play setup (paddles, puck)
     paddle1 = this.physics.add.sprite(50, this.game.config.height / 2, 'paddle');
     paddle1.setScale(0.3); // Scale down to fit the circular boundary
     paddle1.body.setCircle(30); // Set circular collision boundary
@@ -54,8 +112,8 @@ function create() {
         'right': Phaser.Input.Keyboard.KeyCodes.D
     });
 
-    var scoreText = this.add.text(this.game.config.width / 2, 16, '0 - 0', { fontSize: '32px', fill: '#FFF' }).setOrigin(0.5, 0);
-    // scoreText.setClassName('score-text'); // TODO: debug why this 1. doesn't override CSS styles and 2. why this prevents the WASD keys from moving the game play
+
+    scoreText = this.add.text(this.game.config.width / 2, 16, '0 - 0', { fontSize: '32px', fill: '0x000000' }).setOrigin(0.5, 0);
 }
 
 function update() {
